65b33b80f1320fa9e99c20ffb6350492d2b30f89
nzo66 <nzo66.business@gmail.com>
2025-04-23 16:18:34 +0200

    lista.py aktualisieren

--- lista.py
+++ lista.py
@@ -307,7 +307,7 @@ def eventi_m3u8_generator():
     
                         for channel in event["channels"]:
                             m3u8_content += f"#EXTINF:-1 tvg-id=\"{channel['channel_id']}\" tvg-name=\"{tvg_name}\" group-title=\"Eventi\" tvg-logo=\"\", {tvg_name}\n"
-                            m3u8_content += f"{channel['stream_url']}\n"
+                            m3u8_content += f"{channel['stream_url']}&h_user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36&h_referer=https://ilovetoplay.xyz/&h_origin=https://ilovetoplay.xyz\n"
     
         return m3u8_content
     
