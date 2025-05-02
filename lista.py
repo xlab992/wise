c1ca0d524cd870db7d233828b156f23ad728902e
nzo66 <nzo66.business@gmail.com>
2025-05-02 02:01:09 +0200

    lista.py aktualisieren

--- lista.py
+++ lista.py
@@ -39,7 +39,7 @@ def merger_playlist():
             playlist_lines = playlist.splitlines()
             for i in range(len(playlist_lines)):
                 if '.m3u8' in playlist_lines[i]:
-                    playlist_lines[i] += "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Filovetoplay.xyz%2F&h_origin=https%3A%2F%2Filovetoplay.xyz"
+                    playlist_lines[i] += ""
             playlist = '\n'.join(playlist_lines)
     
         if exclude_group_title:
