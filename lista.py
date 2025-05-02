732f313e260a01890e77218c6543d1f45334dc94
nzo66 <nzo66.business@gmail.com>
2025-05-02 14:29:10 +0200

    Update lista.py

--- lista.py
+++ lista.py
@@ -296,7 +296,7 @@ def eventi_m3u8_generator():
     
                         for channel in event["channels"]:
                             m3u8_content += f"#EXTINF:-1 tvg-id=\"{channel['channel_id']}\" tvg-name=\"{tvg_name}\" group-title=\"Eventi\" tvg-logo=\"\", {tvg_name}\n"
-                            m3u8_content += f"https://nzo66-piccolotest.hf.space/proxy/m3u?url={channel['stream_url']}&h_user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36&h_referer=https://ilovetoplay.xyz/&h_origin=https://ilovetoplay.xyz\n"
+                            m3u8_content += f"https://nzo66-piccolotest.hf.space/proxy/m3u?url={channel['stream_url']}&header_user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36&header_referer=https://ilovetoplay.xyz/&header_origin=https://ilovetoplay.xyz\n"
     
         return m3u8_content
     
