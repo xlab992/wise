4f69c8c0e013bef0fb2a1ecace5699fe680749f8
xlab992 <demon_raz92@hotmail.it>
2025-05-12 11:32:03 +0200

    Update lista.py

--- lista.py
+++ lista.py
@@ -51,7 +51,7 @@ def merger_playlist():
     lista = playlist1 + "\n" + playlist2 + "\n" + playlist3
     
     # Aggiungi intestazione EPG
-    lista = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/nzo66/TV/refs/heads/main/epg.xml"\n' + lista
+    lista = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/xlab992/wise/refs/heads/main/epg.xml"\n' + lista
     
     # Salva la playlist
     output_filename = os.path.join(script_directory, "lista.m3u")
@@ -294,7 +294,7 @@ def eventi_m3u8_generator():
     
                         for channel in event["channels"]:
                             m3u8_content += f"#EXTINF:-1 tvg-id=\"{channel['channel_id']}\" tvg-name=\"{tvg_name}\" group-title=\"Eventi\" tvg-logo=\"\", {tvg_name}\n"
-                            m3u8_content += f"https://nzo66-piccolotest.hf.space/proxy/m3u?url={channel['stream_url']}&h_user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36&h_referer=https://ilovetoplay.xyz/&h_origin=https://ilovetoplay.xyz\n"
+                            m3u8_content += f"https://xlab992-bb1.hf.space//proxy/hls/manifest.m3u8?api_password=gf4&d={channel['stream_url']}&h_user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36&h_referer=https://ilovetoplay.xyz/&h_origin=https://ilovetoplay.xyz\n"
     
         return m3u8_content
     
@@ -733,7 +733,7 @@ def vavoo_italy_channels():
                     tvg_id = channel_id_map.get(normalized_name, "")
                     tvg_logo = logos_dict.get(tvg_name_cleaned.lower(), DEFAULT_TVG_ICON)
                     f.write(f'#EXTINF:-1 tvg-id="{tvg_id}" tvg-name="{tvg_name_cleaned}" tvg-logo="{tvg_logo}" group-title="{category}", {name}\n')
-                    f.write(f"https://nzo66-piccolotest.hf.space/proxy/m3u?url={url}\n\n")
+                    f.write(f"https://xlab992-bb1.hf.space//proxy/hls/manifest.m3u8?api_password=gf4&d={url}\n\n")
     
     def main():
         epg_root = fetch_epg(EPG_FILE)
